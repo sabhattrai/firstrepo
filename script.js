54a3124e9c36710376abd1515f35ec32134384b2
sabhattrai <technophile911@gmail.com>
2023-01-04 15:05:09 +0545

    First JS and CSS

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+let num=document.getElementById("num");
+
+
+let count=0;
+function increment(){
+    count=count+1;
+    num.innerText=count;
+
+}
+function decrement(){
+    count=count-1;
+    num.innerText=count;
+
+}
+function give(){
+    num.innerHTML="hello";
+}
+
+
+
